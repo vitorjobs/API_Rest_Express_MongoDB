e59fe72390cbf53f15551d75aa1905ba4aca32b4
vitorjobs <vittorjobs@gmail.com>
2023-01-15 20:43:57 -0300

    FeaT: Primeira Rota para Testar recurso da URL

---
+++ server.js
@@ -0,0 +1,19 @@
+const http = require("http")
+const port = 3000;
+
+const rotas = {
+  '/': 'Curso de NodeJs',
+  '/livros': 'Entrei na Página de Livros',
+  '/autores': 'Listagem de Autores'
+}
+
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end(rotas[req.url]);
+})
+
+// Apontamento do Servidor com Log
+server.listen(port, () => {
+  console.log(`Servidor escutando em http://localhost:${port}`)
+})
